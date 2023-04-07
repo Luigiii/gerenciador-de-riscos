66ad3e7e094a908b05fbff537c97e74314600b1c
Luigi <luigi.eduardo14@gmail.com>
2023-04-07 11:53:25 -0300

    Endpoints para busca dos riscos (todos e por id)

--- src/main/java/poc/oneracao/controller/RiscosAnaliticosController.java
+++ src/main/java/poc/oneracao/controller/RiscosAnaliticosController.java
@@ -4,10 +4,9 @@ import poc.oneracao.repository.RiscoAnaliticoSyncService;
 import poc.oneracao.model.RiscoAnalitico;
 
 import javax.inject.Inject;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 //FIXME: Classe temporária utilizada somente para testar o dynamo localmente
 @Path("/v1/riscos")
@@ -23,4 +22,17 @@ public class RiscosAnaliticosController {
         return riscoAnalitico.toString();
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String buscaRisco(@QueryParam("idRisco") String idRisco) {
+        RiscoAnalitico risco = riscoAnaliticoSyncService.get(idRisco);
+        return risco.toString();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String buscaTodosOsRiscos() {
+        List<RiscoAnalitico> riscos = riscoAnaliticoSyncService.findAll();
+        return riscos.toString();
+    }
 }
